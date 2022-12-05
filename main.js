58256dd5f90943ffd3ff3f48e7e716998cc64ebe
M868-2 <113375937+M868-2@users.noreply.github.com>
2022-12-05 11:23:11 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,24 +4,29 @@ document.getElementById("magicBall-img") .addEventListener("click" , imgclicked)
 
 // VARIABLES
 let outputEl = document.getElementById("output");
+let qn = document.getElementById("question").value.toLowerCase();
 
 function imgclicked () {
-     let ansRand = Math.random;
-     if (ansRand >= 0.1 && ansRand <= 0.99) {
-        if (ansRand <= 0.18) {
-            outputEl.innerHTML = "Without a doubt";
-        } else if (ansRand <= 0.36) {
-            outputEl.innerHTML = "As I see it yes";
-        } else if (ansRand <= 0.54) {
-            outputEl.innerHTML = "Concentrate and ask again";
-        } else if (ansRand <= 0.72) {
-            outputEl.innerHTML = "Don't count on it"
-        } else if (ansRand <= 0.9) {
-            outputEl.innerHTML
-        } else {
-            outputEl.innerHtML = "Yes";
-        }
-     }    else {
-        outputEl.innerHTML = "Please ask again";
+    let ansRand = Math.random;
+    if (qn === "---") {
+        outputEl.innerHTML = ansRand;
+          } if (ansRand < 0.20) {
+                outputEl.innerHTML = "Without a doubt";
+          } else if (ansRand < 0.40) {
+                outputEl.innerHTML = "As I see it, yes";
+          } else if (ansRand < 0.60) {
+                outputEl.innerHTML = "Concentrate and ask again";
+         } else if (ansRand < 0.80) {
+                outputEl.innerHTML = "Don't count on it";
+         } else {
+                outputEl.innerHTML = "Outlook not so good";
+     } else {
+        outputEl.innerHTML = "Please ask again"
      }
+ 
 }
+
+
+
+
+
