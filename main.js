feb9658804dc3ee622e800048d155a883d692dc2
Jaisleen Mankoo <j.mankoo@EDU.epsb.ca>
2023-09-19 13:15:01 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,38 +4,43 @@ document.getElementById("magicBall-img").addEventListener("click", imgclicked);
 
 function imgclicked() {
   // VARIABLES
-  let outputEl = document.getElementById("output");
   let qn = document.getElementById("question").value.toLowerCase();
 
-  // else {
-  //   // Random Response
-  //   let ansRand = Math.random();
-  //   if (ansRand < 0.35) {
-  //     outputEl.innerHTML = "How am I supposed to know?";
-  //   } else if (ansRand < 0.45) {
-  //     outputEl.innerHTML = "Girl just no";
-  //   } else if (ansRand < 0.55) {
-  //     outputEl.innerHTML = "You're not concentrating :|";
-  //   } else if (ansRand < 0.65) {
-  //     outputEl.innerHTML = "Go get a life";
-  //   } else if (ansRand < 0.75) {
-  //     outputEl.innerHTML = "Ooo";
-  //   } else if (ansRand < 0.85) {
-  //     outputEl.innerHTML = "Seek help";
-  //   } else if (ansRand < 0.95) {
-  //     outputEl.innerHTML = "Umm";
-  //   } else {
-  //     outputEl.innerHTML = "Have a great day";
-  //   }
-  // }
+  // OUTPUT
+  document.getElementById("output").innerHTML = getResponse(qn);
+
 }
 
-function getResponse(qn) {
-  if (qn === "does the magic 8 ball work" || qn === "does this 8 ball work") {
-    outputEl.innerHTML = "Does your brain work?";
-  } else if (qn === "is js awesome" || qn === "is javascript awesome" ) {
-    
-  } else if (qn == "") {
-    outputEl.innerHTML = "Please ask a question";
-  } 
+function getResponse(qn1) {
+  if (qn1 === "does the magic 8 ball work" || qn1 === "does this 8 ball work") {
+    return "Does your brain work?";
+  } else if (qn1 === "is js awesome" || qn1 === "is javascript awesome" ) {
+    return "Of course it is!";
+  } else if (qn1 == "") {
+    return "Please ask a question";
+  } else {
+    return randomResponse();
+  }
+}
+
+function randomResponse() {
+  let ansRand = Math.random();
+
+  if (ansRand < 0.35) {
+    return "How am I supposed to know?";
+  } else if (ansRand < 0.45) {
+    return "Girl just no";
+  } else if (ansRand < 0.55) {
+    return "You're not concentrating :|";
+  } else if (ansRand < 0.65) {
+    return "Without a doubt";
+  } else if (ansRand < 0.75) {
+    return "did you know blue is the best color?";
+  } else if (ansRand < 0.85) {
+    return "Don't do it";
+  } else if (ansRand < 0.95) {
+    return "Bonjour, je m'appelle Luoisa, ta boule magique 8";
+  } else {
+    return "Have a great day";
+  }
 }
